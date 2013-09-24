96373ac9c19229a6b31f8e21614ae437c4cd6465
App Academy Student <student@appacademy.io>
2013-09-24 15:08:14 -0700

    Add board to_s method

--- chess.rb
+++ chess.rb
@@ -1,4 +1,36 @@
+require './chess_piece.rb'
+
 class Board
+  attr_accessor :state
+
+  def initialize
+    @state = Array.new(8) { Array.new(8) }
+    setup
+  end
+
+
+  def setup
+    pawn = Pawn.new(self, :black)
+    pawn.position = [1, 0]
+    @state[1][0] = pawn
+  end
+
+  def to_s
+    @state.each_with_index do |row, index_r|
+      row.each_with_index do |column, index_c|
+        square = @state[index_r][index_c]
+        if square
+          print square
+        else
+          print " #{@state[index_r][index_c].inspect} "
+        end
+      end
+
+      puts
+    end
+  end
+
+
 end
 
 class HumanPlayer
@@ -6,7 +38,3 @@ end
 
 class Game
 end
-
-class Piece
-end
-
