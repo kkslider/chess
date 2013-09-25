fefa91a97fad84c872c03086e117ba7ee51a2156
App Academy Student <student@appacademy.io>
2013-09-25 16:30:16 -0700

    Refactor board move and checks

--- chess.rb
+++ chess.rb
@@ -1,5 +1,8 @@
 require './chess_piece.rb'
 require 'yaml'
+#
+# class ImproperPieceError < StandardError
+# end
 
 class Board
   attr_accessor :state
@@ -26,14 +29,19 @@ class Board
 
   def setup_black_pieces
     setup_pawns(BLACK_ROW_PAWNS)
-    @state[0][0] = Rook.new(self, :black, [0, 0])
+    # @state[0][0] = Rook.new(self, :black, [0, 0])
     @state[0][1] = Knight.new(self, :black, [0, 1])
     @state[0][2] = Bishop.new(self, :black, [0, 2])
-    @state[0][3] = Queen.new(self, :black, [0, 3])
+    # @state[0][3] = Queen.new(self, :black, [0, 3])
     @state[0][4] = King.new(self, :black, [0, 4])
     @state[0][5] = Bishop.new(self, :black, [0, 5])
     @state[0][6] = Knight.new(self, :black, [0, 6])
-    @state[0][7] = Rook.new(self, :black, [0, 7])
+    # @state[0][7] = Rook.new(self, :black, [0, 7])
+
+
+    @state[4][1] = Rook.new(self, :black, [4, 1])
+    @state[2][1] = Rook.new(self, :black, [2, 1])
+    @state[2][0] = Queen.new(self, :black, [2, 0])
   end
 
   def setup_white_pieces
@@ -42,10 +50,59 @@ class Board
     @state[7][1] = Knight.new(self, :white, [7, 1])
     @state[7][2] = Bishop.new(self, :white, [7, 2])
     @state[7][3] = Queen.new(self, :white, [7, 3])
-    @state[7][4] = King.new(self, :white, [7, 4])
+    # @state[7][4] = King.new(self, :white, [7, 4])
     @state[7][5] = Bishop.new(self, :white, [7, 5])
     @state[7][6] = Knight.new(self, :white, [7, 6])
     @state[7][7] = Rook.new(self, :white, [7, 7])
+
+    @state[3][3] = King.new(self, :white, [3, 3])
+  end
+
+
+  def get_piece_for_current_player(current_player)
+    begin
+      puts "#{current_player}, your turn. Enter position of the piece you " +
+      "would like to move. e.g. '1 2' for position at row 2, column 3"
+
+      move_from = gets.chomp.split.map(&:to_i) #[1, 2]
+      piece = self.state[move_from[0]][move_from[1]]
+      raise if (!piece || piece.color != current_player.color || piece.valid_moves.empty?)
+
+    rescue
+      puts "Please choose a proper piece."
+      retry
+    end
+
+    piece
+  end
+
+  def move_piece(piece, current_player)
+    begin
+      puts "#{current_player}, Enter the position you would like to move " +
+      "the #{piece.class}."
+
+      move_from = piece.position
+      move_to = gets.chomp.split.map(&:to_i)
+
+      until piece.valid_moves.include?(move_to)
+        puts "#{current_player}, Enter the position you would like to move " +
+        "the #{piece.class}."
+        move_to = gets.chomp.split.map(&:to_i)
+      end
+
+      piece.position = move_to
+      self.state[move_to[0]][move_to[1]] = piece
+      self.state[move_from[0]][move_from[1]] = nil
+
+      if piece.class == Pawn
+        piece.initial_move = false
+      end
+
+      # print self
+    rescue => e
+      puts "Please enter valid coordinates."
+      retry
+    end
   end
 
   # def move(start, end)
@@ -135,19 +192,15 @@ class Board
   end
 
   def checkmate?(color)
-    # opp_color = (color == :white) ? :black : :white
-    # serialized_board = self.to_yaml
     serialized_board = self.to_yaml
     temp_board = YAML::load(serialized_board)
 
     temp_board.state.each_with_index do |row, r_index|
       row.each_with_index do |column, c_index|
         piece = temp_board.state[r_index][c_index]
-        # serialized_board = self.to_yaml
 
         if piece && piece.color == color
           piece.valid_moves.each do |valid_move|
-            #check if valid_move results in check
             current_x, current_y = piece.position[0], piece.position[1]
             new_x, new_y = valid_move[0], valid_move[1]
             temp_board.state[current_x][current_y] = nil
@@ -157,14 +210,13 @@ class Board
               return false
             end
 
-
-            # serialized_board = self.to_yaml
+            serialized_board = self.to_yaml
             temp_board = YAML::load(serialized_board)
-            # self = YAML::load(serialized_board)
           end
         end
       end
     end
+
 
     true
   end
@@ -208,7 +260,6 @@ class HumanPlayer
     @color == :white ? "Player 1 (W)" : "Player 2 (B)"
   end
 
-
 end
 
 class ChessGame
@@ -232,10 +283,7 @@ class ChessGame
     until game_over
       serialized_board = board.to_yaml
 
-      # board.move
-
       if board.checked?(@current_player.color)
-        # for each piece, check each valid move, and if all the valid moves result in checked, you are in checkmate
         if board.checkmate?(@current_player.color)
           game_over = true
           next
@@ -244,48 +292,15 @@ class ChessGame
         puts "YO KING IS IN CHECK!! DO SUMTHIN"
       end
 
-      begin
-      puts "#{current_player}, your turn. Enter position of the piece you " +
-        "would like to move. e.g. '1 2' for position at row 2, column 3"
+      piece_from = board.get_piece_for_current_player(@current_player)
+      board.move_piece(piece_from, @current_player)
 
-        move_from = gets.chomp.split.map(&:to_i) #[1, 2]
-        piece = board.state[move_from[0]][move_from[1]]
-      rescue
-        puts "please re-enter"
-        retry
+      if board.checked?(@current_player.color)
+        puts "You put yourself in check! Please retry that move."
+        board = YAML::load(serialized_board)
+        next
       end
-      next if !piece || piece.color != @current_player.color
-
-      begin
-      puts "#{current_player}, Enter the position you would like to move " +
-      "the #{piece.class}."
-
-        move_to = gets.chomp.split.map(&:to_i)
-
-        if piece.valid_moves.include?(move_to)
-          piece.position = move_to
-          board.state[move_to[0]][move_to[1]] = piece
-          board.state[move_from[0]][move_from[1]] = nil
-
-          if piece.class == Pawn
-            piece.initial_move = false
-          end
-
-          if board.checked?(@current_player.color)
-            puts "YOU MADE A BAAAAAD MOVE SON"
-            board = YAML::load(serialized_board)
-            next
-          end
-
-          print board
-        else
-          next
-        end
-      rescue
-        puts "please re-enter"
-        retry
-      end
-
+      print board
       @current_player = (@current_player == player_1) ? player_2 : player_1
     end
 
